4f5d990f74881c07d506a7dc8c28e40e008d38ee
RouwenR <rouwenx@gmail.com>
2023-03-15 08:49:27 +0700

    pusing palak aku

--- controllers/ProdukController.php
+++ controllers/ProdukController.php
@@ -1,21 +1,5 @@
 <?php
-
-$conn = mysqli_connect('localhost', 'root', '', 'db_penjualan');
-
-function query($query)
-{
-  global $conn;
-  $result = mysqli_query($conn, $query);
-  $rows = [];
-
-  while ($row = mysqli_fetch_assoc($result)) {
-    $rows[] = $row;
-  }
-
-  return $rows;
-}
-
-
+include "../../function.php";
 /**
  * Insert barang
  */
--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<?php
-require 'controllers/ProdukController.php';
-$dataProduk = query("SELECT * FROM tbproduk");
-
-if (isset($_POST["cari"])) {
-  $dataProduk = dataProduk($_POST["cari"]);
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -31,7 +22,7 @@ if (isset($_POST["cari"])) {
             <a class="nav-link active text-light" aria-current="page" href="views/produk/produk.php">Produk</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link text-light" href="#">Transaksi</a>
+            <a class="nav-link text-light" href="views/transaksi/transaksi.php">Transaksi</a>
           </li>
         </ul>
       </div>
@@ -41,51 +32,6 @@ if (isset($_POST["cari"])) {
   <div class="container">
     <h1 class="mt-3">Welcome To JRV Mart Pontianak</h1>
     <p>See our product here <a href="views/produk/produk.php">Produk YPS</a></p>
-  </div>
-
-  <div class="container">
-    <h4>List Barang</h4>
-
-    <div class="form-group">
-      <form action="" method="POST">
-        <div class="row">
-          <div class="input-group mb-2">
-            <input type="text" name="cari" class="form-control" autofocus placeholder="Masukkan cari pencarian" autocomplete="off" id="cari">
-            <button class="btn btn-info" type="submit" name="cari" id="tombol-cari">Cari!</button>
-          </div>
-      </form>
-    </div>
-    <div>
-      <table class="table table-bordered border-primary text-center mt-2">
-        <tr>
-          <th>No.</th>
-          <th>Nama Produk</th>
-          <th>Kategori</th>
-          <th>Harga Jual</th>
-          <th>Aksi</th>
-        </tr>
-
-        <?php $i = 1;
-        foreach ($dataProduk as $row) : ?>
-          <tr>
-            <td><?= $row['idProduk'] ?></td>
-            <td><?= $row['namaProduk'] ?></td>
-            <td><?= $row['kategori'] ?></td>
-            <td><?= $row['hargaJual'] ?></td>
-            <td><a class="btn btn-success " href="">Add to Cart</a></td>
-          <?php endforeach; ?>
-          </tr>
-      </table>
-    </div>
-  </div>
-
-
-
-  <div class>
-    <tr>
-      <td><b>Tanggal</b></td>
-      <td><input type="text" readonly="readonly" class="form-control" value="<?php echo date("j F Y, G:i"); ?>" name="tgl"></td>
-    </tr>
   </div>
 </body>
 
