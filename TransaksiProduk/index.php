5586fd991d52561c7cb77addce721ac83129233f
JovBntro <jovanbuntoro66@gmail.com>
2023-03-16 07:42:03 +0700

    wqesdfdedwdafs

--- TransaksiProduk/index.php
+++ TransaksiProduk/index.php
@@ -13,16 +13,33 @@
 </head>
 
 <body>
-    <div class="container">
-    <div class="page-header">
-        <h1>Welcome to JRV Market</h1>
+
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">JRV Mart</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="views/produk/produk.php">Produk</a>
+                    <a class="nav-link" href="views/transaksi/transaksi.php">Transaksi</a>
+                    <a class="nav-link" href="views/struk/struk.php">Struk</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+    <div class="container mt-3">
+        <div class="page-header">
+            <h1>Welcome to JRV Market</h1>
+        </div>
+        <ul>
+            <li><a href="views/produk/produk.php">Our Products</a></li>
+            <li><a href="#">Transaksi</a></li>
+            <li><a href="#">Struk</a></li>
+        </ul>
     </div>
-    <ul>
-         <li><a href="views/produk/produk.php">Our Products</a></li>
-        <li><a href="#">Transaksi</a></li>
-        <li><a href="#">Struk</a></li>
-    </ul>
-</div>
 </body>
 
 </html>
--- views/produk/delete.php
+++
@@ -1,5 +0,0 @@
-<?php
-require "../../controllers/ProdukController.php";
-if (deleteProduk($_GET['id']) > 0) {
-    header("Location: produk.php");
-}
